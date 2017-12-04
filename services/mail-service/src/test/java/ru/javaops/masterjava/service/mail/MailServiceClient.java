b86fb867c9553e6d7cd5a8206a1cd233ac4eabcd
gkislin <javawebinar@yandex.ru>
2017-12-04 17:34:45 +0300

    8_3_HW7_update_wsdl

--- services/mail-service/src/test/java/ru/javaops/masterjava/service/mail/MailServiceClient.java
+++ services/mail-service/src/test/java/ru/javaops/masterjava/service/mail/MailServiceClient.java
@@ -15,7 +15,14 @@ public class MailServiceClient {
                 new QName("http://mail.javaops.ru/", "MailServiceImplService"));
 
         MailService mailService = service.getPort(MailService.class);
-        mailService.sendToGroup(ImmutableSet.of(
-                new Addressee("masterjava@javaops.ru", null)), null, "Subject", "Body");
+
+        String state = mailService.sendToGroup(ImmutableSet.of(new Addressee("masterjava@javaops.ru", null)), null,
+                "Group mail subject", "Group mail body");
+        System.out.println("Group mail state: " + state);
+
+        GroupResult groupResult = mailService.sendBulk(ImmutableSet.of(
+                new Addressee("Мастер Java <masterjava@javaops.ru>"),
+                new Addressee("Bad Email <bad_email.ru>")), "Bulk mail subject", "Bulk mail body");
+        System.out.println("\nBulk mail groupResult:\n" + groupResult);
     }
 }
