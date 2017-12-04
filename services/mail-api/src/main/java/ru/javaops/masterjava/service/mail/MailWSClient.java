7391f34368f802eaa46025d9474a5abf586f97ab
gkislin <javawebinar@yandex.ru>
2017-12-04 22:49:51 +0300

    8_4_HW7_webapp

--- services/mail-api/src/main/java/ru/javaops/masterjava/service/mail/MailWSClient.java
+++ services/mail-api/src/main/java/ru/javaops/masterjava/service/mail/MailWSClient.java
@@ -1,5 +1,8 @@
 package ru.javaops.masterjava.service.mail;
 
+import com.google.common.base.Splitter;
+import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Iterables;
 import com.google.common.io.Resources;
 import lombok.extern.slf4j.Slf4j;
 import ru.javaops.masterjava.web.WsClient;
@@ -20,8 +23,22 @@ public class MailWSClient {
     }
 
 
-    public static void sendToGroup(final Set<Addressee> to, final Set<Addressee> cc, final String subject, final String body) {
-        log.info("Send mail to '" + to + "' cc '" + cc + "' subject '" + subject + (log.isDebugEnabled() ? "\nbody=" + body : ""));
-        WS_CLIENT.getPort().sendToGroup(to, cc, subject, body);
+    public static String sendToGroup(final Set<Addressee> to, final Set<Addressee> cc, final String subject, final String body) {
+        log.info("Send to group to '" + to + "' cc '" + cc + "' subject '" + subject + (log.isDebugEnabled() ? "\nbody=" + body : ""));
+        String status = WS_CLIENT.getPort().sendToGroup(to, cc, subject, body);
+        log.info("Send to group with status: " + status);
+        return status;
+    }
+
+    public static GroupResult sendBulk(final Set<Addressee> to, final String subject, final String body) {
+        log.info("Send bulk to '" + to + "' subject '" + subject + (log.isDebugEnabled() ? "\nbody=" + body : ""));
+        GroupResult result = WS_CLIENT.getPort().sendBulk(to, subject, body);
+        log.info("Sent bulk with result: " + result);
+        return result;
+    }
+
+    public static Set<Addressee> split(String addressees) {
+        Iterable<String> split = Splitter.on(',').trimResults().omitEmptyStrings().split(addressees);
+        return ImmutableSet.copyOf(Iterables.transform(split, Addressee::new));
     }
 }
---
+++ web/webapp/src/main/java/ru/javaops/masterjava/webapp/SendServlet.java
@@ -0,0 +1,36 @@
+package ru.javaops.masterjava.webapp;
+
+import lombok.extern.slf4j.Slf4j;
+import ru.javaops.masterjava.service.mail.GroupResult;
+import ru.javaops.masterjava.service.mail.MailWSClient;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/send")
+@Slf4j
+public class SendServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String result;
+        try {
+            log.info("Start sending");
+            req.setCharacterEncoding("UTF-8");
+            resp.setCharacterEncoding("UTF-8");
+            String users = req.getParameter("users");
+            String subject = req.getParameter("subject");
+            String body = req.getParameter("body");
+            GroupResult groupResult = MailWSClient.sendBulk(MailWSClient.split(users), subject, body);
+            result = groupResult.toString();
+            log.info("Processing finished with result: {}", result);
+        } catch (Exception e) {
+            log.error("Processing failed", e);
+            result = e.toString();
+        }
+        resp.getWriter().write(result);
+    }
+}
