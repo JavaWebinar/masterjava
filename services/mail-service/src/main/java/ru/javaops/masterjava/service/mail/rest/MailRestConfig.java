1c89512415ac9d695b054084aebb3b0a606d1824
gkislin <javawebinar@yandex.ru>
2017-12-18 20:15:34 +0300

    10_4_jersey_logging

--- services/mail-service/src/main/java/ru/javaops/masterjava/service/mail/rest/MailRestConfig.java
+++ services/mail-service/src/main/java/ru/javaops/masterjava/service/mail/rest/MailRestConfig.java
@@ -1,12 +1,17 @@
 package ru.javaops.masterjava.service.mail.rest;
 
 import org.glassfish.jersey.server.ResourceConfig;
+import org.slf4j.bridge.SLF4JBridgeHandler;
 
 import javax.ws.rs.ApplicationPath;
 
 @ApplicationPath("rest")
 public class MailRestConfig extends ResourceConfig {
+
     public MailRestConfig() {
+        // Set Jersey log to SLF4J instead of JUL
+        // http://stackoverflow.com/questions/4121722
+        SLF4JBridgeHandler.install();
         packages("ru.javaops.masterjava.service.mail.rest");
     }
 }
